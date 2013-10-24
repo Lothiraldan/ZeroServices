69113ac6e42feff0dc7a0a358a25880bab017416
FELD Boris <lothiraldan@gmail.com>
2013-10-25 00:10:53 +0200

    WIP on chat example

--- examples/chat.py
+++ examples/chat.py
@@ -1,5 +1,14 @@
+from zmq.eventloop import ioloop, zmqstream
+ioloop.install()
+
+import json
+
 from zeroservices import BaseService
 from zeroservices import ZeroMQMedium
+
+from tornado import gen
+from tornado import web
+from tornado import websocket
 
 from time import time
 
@@ -17,16 +26,46 @@ class ChatService(BaseService):
         print "ON EVENT", message_type, message
 
     def on_new_node(self, node_info):
-        print "A NEW CHALLENGER !", node_info
+        msg = json.dumps({'type': 'new_user', 'id': node_info['node_id'], 'name': node_info['name']})
+        self.send_to_all_clients(msg)
 
-    def coucou(self):
-        self.medium.publish('Hello', {'message': 'World'})
+    def send_to_all_clients(self, msg):
+        for client in clients:
+            client.write_message(msg)
 
-    def main(self):
-        self.medium.ioloop.add_timeout(time() + 2, self.coucou)
-        super(ChatService, self).main()
+
+clients = []
+
+
+class MainHandler(web.RequestHandler):
+    def get(self):
+        return self.render('chat.html', port=int(sys.argv[2]))
+
+
+class WebSocketHandler(websocket.WebSocketHandler):
+
+    def open(self):
+        clients.append(self)
+        print "OPEN"
+        msg = 'Welcome %s' % sys.argv[1]
+        self.write_message(json.dumps({'type': 'message', 'message': msg}))
+
+    def on_close(self):
+        clients.remove(self)
+
+
+urls = [
+    (r"/", MainHandler),
+    (r"/websocket", WebSocketHandler)]
 
 if __name__ == '__main__':
     import sys
     s = ChatService(sys.argv[1])
-    s.main()
+    s.medium.register()
+
+    application = web.Application(urls)
+    application.listen(int(sys.argv[2]))
+    try:
+        ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print 'Interrupted'

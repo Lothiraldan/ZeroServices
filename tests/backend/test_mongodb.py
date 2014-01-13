7c6d8ad8fcbcc096683cd6934b8cfbd7050f0b3a
FELD Boris <lothiraldan@gmail.com>
2014-01-13 21:36:35 +0100

    Move mongodb test to the right folder

--- tests/backend/test_mongodb.py
+++ tests/backend/test_mongodb.py
@@ -16,12 +16,76 @@ class MongoDBCollectionTestCase(unittest.TestCase):
         self.collection = MongoDBCollection(self.ressource_name)
         self.collection.service = self.service
 
-    def test_create(self):
-        ressource_id = 'UUID-1'
-        ressource_data = {'field1': 1, 'field2': 2}
+        # Ressource
+        self.ressource_id = 'UUID-1'
+        self.ressource_data = {'field1': 1, 'field2': 2}
 
-        message = {'action': 'create', 'ressource_id': ressource_id,
-                   'ressource_data': ressource_data}
+    def tearDown(self):
+        self.collection.collection.drop()
+
+    def test_create(self):
+        message = {'action': 'create', 'ressource_id': self.ressource_id,
+                   'ressource_data': self.ressource_data}
 
         self.assertEqual(self.collection.on_message(**message),
-                         {'ressource_id': ressource_id})
+                         {'ressource_id': self.ressource_id})
+
+    def test_get(self):
+        self.test_create()
+
+        message = {'action': 'get', 'ressource_id': self.ressource_id}
+
+        self.assertEqual(self.collection.on_message(**message),
+                         {'ressource_id': self.ressource_id,
+                          'ressource_data': self.ressource_data})
+
+    def test_update(self):
+        self.test_create()
+
+        patch = {'field3': 3, 'field4': 4}
+        query = {'$set': patch}
+
+        message = {'action': 'patch', 'ressource_id': self.ressource_id,
+                   'patch': query}
+
+        expected_document = self.ressource_data.copy()
+        expected_document.update(patch)
+        expected_document['_id'] = self.ressource_id
+
+        self.assertEqual(self.collection.on_message(**message),
+            expected_document)
+
+    def test_delete(self):
+        self.test_get()
+
+        message = {'action': 'delete', 'ressource_id': self.ressource_id}
+
+        self.assertEqual(self.collection.on_message(**message),
+                         'OK')
+
+
+        message = {'action': 'get', 'ressource_id': self.ressource_id}
+
+        self.assertEqual(self.collection.on_message(**message),
+                         'NOK')
+
+    def test_add_link(self):
+        self.test_create()
+
+        relation = 'relation_type'
+        target_id = 'target'
+        title = 'title'
+        message = {'action': 'add_link', 'ressource_id': self.ressource_id,
+                   'relation': relation, 'target_id': target_id, 'title': title}
+        self.assertEqual(self.collection.on_message(**message),
+            "OK")
+
+        expected_data = self.ressource_data.copy()
+        expected_data.update({'_links': {relation: [
+                            {"target_id": target_id, "title": title}]}})
+        expected_document = {'ressource_id': self.ressource_id,
+                             'ressource_data': expected_data}
+
+        message = {'action': 'get', 'ressource_id': self.ressource_id}
+        self.assertEqual(self.collection.on_message(**message),
+                         expected_document)

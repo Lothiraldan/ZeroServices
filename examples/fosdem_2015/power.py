6810fdd7156b9839acee7a27aae57041850603e0
FELD Boris <boris.feld@tinyclues.com>
2015-02-02 23:15:59 +0100

    Add missing file in fosdem 2015 example

---
+++ examples/fosdem_2015/power.py
@@ -0,0 +1,15 @@
+from zeroservices import ZeroMQMedium, ResourceService
+from zeroservices.backend.mongodb import MongoDBCollection
+
+
+class PowerCollection(MongoDBCollection):
+
+    def __init__(self, *args, **kwargs):
+        super(PowerCollection, self).__init__(*args, **kwargs)
+        self.collection.ensure_index([('description', 'text')])
+
+
+if __name__ == '__main__':
+    todo = ResourceService('fosdem_2015_power', ZeroMQMedium(port_random=True))
+    todo.register_resource(PowerCollection("power", "fosdem_db"))
+    todo.main()
